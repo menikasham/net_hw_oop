3ffb3be7d3f12ba2847b2d94f69d1fa6db48611e
Menika <menika.sham@gmail.com>
2025-03-18 15:46:42 +0300

    tasks done

--- homework.py
+++ homework.py
@@ -1,5 +1,6 @@
 from statistics import median
 
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -18,17 +19,37 @@ class Student:
         else:
             return 'Error'
 
-    def avg_rate(self, course):
-        if course in self.grades:
-            avg_r = median(self.grades[course])
-            return avg_r
-        return 0
+    @property
+    def avg_rate(self):
+        all_grades = [grade for course_grades in self.grades.values() for grade in course_grades]
+        return round(sum(all_grades) / len(all_grades), 2) if all_grades else 0
+
+    def __lt__(self, other):
+        return self.avg_rate < other.avg_rate
+
+    def __le__(self, other):
+        return self.avg_rate <= other.avg_rate
+
+    def __eq__(self, other):
+        return self.avg_rate == other.avg_rate
+
+    def __ne__(self, other):
+        return self.avg_rate != other.avg_rate
+
+    def __gt__(self, other):
+        return self.avg_rate > other.avg_rate
+
+    def __ge__(self, other):
+        return self.avg_rate >= other.avg_rate
 
     def __str__(self):
+        all_grades = [grade for course_grades in self.grades.values() for grade in course_grades]
+        avg_rate = median(all_grades) if all_grades else 0
         return (f'Имя: {self.name}\nФамилия: {self.surname}\n'
-                f'Средняя оценка за домашние задания: {self.avg_rate}\n'
+                f'Средняя оценка за домашние задания: {avg_rate}\n'
                 f'Курсы в процессе изучения: {self.courses_in_progress}\n'
                 f'Завершенные курсы: {self.finished_courses}')
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -36,19 +57,40 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
 
-    def avg_lecture_rate(self, course):
-        if course in self.grades:
-            return median(self.grades[course])
-        return 0
-
     def __str__(self):
-        return (f'Имя: {self.name}\nФамилия: {self.surname}\n'
-                f'Средняя оценка за лекцию: {self.avg_lecture_rate}')
+        all_grades = [grade for course_grades in self.grades.values() for grade in course_grades]
+        avg_rate = median(all_grades) if all_grades else 0
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {avg_rate}'
+
+    @property
+    def avg_rate(self):
+        all_grades = [grade for course_grades in self.grades.values() for grade in course_grades]
+        return round(sum(all_grades) / len(all_grades), 2) if all_grades else 0
+
+    def __lt__(self, other):
+        return self.avg_rate < other.avg_rate
+
+    def __le__(self, other):
+        return self.avg_rate <= other.avg_rate
+
+    def __eq__(self, other):
+        return self.avg_rate == other.avg_rate
+
+    def __ne__(self, other):
+        return self.avg_rate != other.avg_rate
+
+    def __gt__(self, other):
+        return self.avg_rate > other.avg_rate
+
+    def __ge__(self, other):
+        return self.avg_rate >= other.avg_rate
+
 
 class Rewiewer(Mentor):
     def __init__(self, name, surname):
@@ -72,20 +114,16 @@ worst_student = Student('Vasilisa', 'Pupkina', 'female')
 best_student.courses_in_progress += ['Python', 'JavaScript']
 worst_student.courses_in_progress += ['Python', 'Java']
 
-
-
 fn_reviewer = Rewiewer('Ya', 'Chumba')
 fn_reviewer.courses_attached += ['Python', 'JavaScript']
 
 sd_reviewer = Rewiewer('Dzen', 'Chumba')
 sd_reviewer.courses_attached += ['Python', 'Java']
 
-
 fn_lector = Lecturer('Basha', 'Buzuki')
 fn_lector.courses_attached += ['Python', 'JavaScript']
 sd_lector = Lecturer('Sam', 'Huang')
 sd_lector.courses_attached += ['Python', 'Java']
-
 
 fn_reviewer.rate_hw(best_student, 'Python', 9)
 fn_reviewer.rate_hw(worst_student, 'Java', 8)
@@ -97,5 +135,33 @@ best_student.rate_lecture(fn_lector, 'JavaScript', 8)
 worst_student.rate_lecture(fn_lector, 'Python', 10)
 worst_student.rate_lecture(sd_lector, 'Java', 9)
 
+print(fn_reviewer, end='\n\n')
+print(fn_lector, end='\n\n')
+print(best_student, end='\n\n')
+print(best_student > worst_student)
+print(fn_lector > sd_lector, end='\n\n')
 
-print(fn_lector.avg_lecture_rate('Python'))
+
+def average_student_grade(students, course):
+    all_grades = []
+    for student in students:
+        if course in student.grades:
+            all_grades.extend(student.grades[course])
+
+    return round(sum(all_grades) / len(all_grades), 2) if all_grades else 0
+
+
+def average_lecturer_grade(lecturers, course):
+    all_grades = []
+    for lector in lecturers:
+        if course in lector.grades:
+            all_grades.extend(lector.grades[course])
+
+    return round(sum(all_grades) / len(all_grades), 2) if all_grades else 0
+
+
+student_lst = [best_student, worst_student]
+lecturers_lst = [fn_lector, fn_lector]
+
+print(average_student_grade(student_lst, 'Python'))
+print(average_lecturer_grade(lecturers_lst, 'Python'))
